e3eceeddd4103af00d95ddf18ee441c6e816af99
Md. Tauhidul Islam <tauhidul.islam@brainstation23.com>
2021-04-26 21:56:59 +0600

    code refactory

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        async getUser(){
+        async getUser() {
             const res = await fetch('https://randomuser.me/api');
             const { results } = await res.json();
 
-            this.firstName= results[0].name.first,
-            this.lastName= results[0].name.last,
-            this.email= results[0].email,
-            this.gender= results[0].gender,
-            this.picture= results[0].picture.large
+            this.firstName = results[0].name.first,
+                this.lastName = results[0].name.last,
+                this.email = results[0].email,
+                this.gender = results[0].gender,
+                this.picture = results[0].picture.large
         }
     }
 });
