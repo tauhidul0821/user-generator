eb21ad8f210ff3b0563b7876ffae18f13357a3d9
Md. Tauhidul Islam <tauhidul.islam@brainstation23.com>
2021-04-26 21:25:07 +0600

    first user show

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'John@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg'
+        }
+    }
+});
+
+app.mount('#app')
